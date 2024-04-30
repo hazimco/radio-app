8547c6076838347ea96790f4644a562768a1e859
hazimco <140416762+hazimco@users.noreply.github.com>
2024-04-30 11:56:27 +0200

    adding Channel component to show channel info, and nesting Programs and Episodes inside route for Channel since channel info is displayed on both of those pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Start from "./components/Start";
+import Channel from "./components/Channel";
 import Programs from "./components/Programs";
 import Episodes from "./components/Episodes";
 
@@ -8,8 +9,10 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Start />} />
-      <Route path="/channel" element={<Programs />} />
-      <Route path="/channel/episodes/:id" element={<Episodes />} />
+      <Route path="/channel" element={<Channel />}>
+        <Route path="" element={<Programs />} />
+        <Route path="episodes/:id" element={<Episodes />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/components/Channel.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Channel = () => {
+  return (
+    <div>
+      <h1>Channel</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Channel;
