2c80d69b872bb7481e36585735481c80957d9620
hazimco <140416762+hazimco@users.noreply.github.com>
2024-05-03 12:17:20 +0200

    styling episode items according to desktop design for screens wider than 640px

--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -6,15 +6,17 @@ import { isDate, getDateMillis } from "../utils/helper";
 
 const Episode = ({ episode }) => {
   return (
-    <li className="py-6">
-      <img className="w-24 mb-5" src={episode.imageurl} alt="" />
-      <div className="text-white text-2xl mb-1">{episode.title}</div>
-      <div className="text-slate-400 text-sm mb-4">{episode.description}</div>
-      <audio
-        controls
-        src={episode.listenpodfile?.url || episode.broadcast?.broadcastfiles}
-        className="h-10 w-full"
-      ></audio>
+    <li className="py-6 sm:flex">
+      <img className="w-24 h-24 mb-5" src={episode.imageurl} alt="" />
+      <div className="sm:ml-6">
+        <div className="text-white text-2xl mb-1">{episode.title}</div>
+        <div className="text-slate-400 text-sm mb-4">{episode.description}</div>
+        <audio
+          controls
+          src={episode.listenpodfile?.url || episode.broadcast?.broadcastfiles}
+          className="h-10 w-full sm:w-1/2"
+        ></audio>
+      </div>
     </li>
   );
 };
