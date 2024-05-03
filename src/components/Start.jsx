51f7bb2369a9c4ce447015ae4c64bdc0c2a4bf26
hazimco <140416762+hazimco@users.noreply.github.com>
2024-05-03 13:47:50 +0200

    adding play icon to Start page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import { PlayCircleIcon } from "@heroicons/react/16/solid";
 
 const Start = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-8">
-      <h1 className="text-pink-300 text-6xl sm:text-8xl font-semibold">
+      <h1 className="text-pink-300 text-6xl sm:text-8xl font-semibold flex gap-5 items-center">
+        <PlayCircleIcon className="size-16 p-4 text-pink-200 bg-pink-600  rounded-full sm:size-24 sm:p-6" />
         Radio app
       </h1>
       <Link
