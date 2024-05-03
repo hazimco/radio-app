4e98ee44b6da6b3e83566b6fab1cdbd7c1641f8a
hazimco <140416762+hazimco@users.noreply.github.com>
2024-05-03 10:22:26 +0200

    making heading on Start page larger on screens that are wider than 640px

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Start = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-8">
-      <h1 className="text-pink-300 text-6xl font-semibold">Radio app</h1>
+      <h1 className="text-pink-300 text-6xl sm:text-8xl font-semibold">
+        Radio app
+      </h1>
       <Link
         to={"/channel"}
         className="text-gray-100 text-center bg-gradient-to-r from-pink-700 to-purple-700 rounded-lg px-6 py-3"
