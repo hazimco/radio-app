c1904db190d7395df919fa59ba820f3187d01817
hazimco <140416762+hazimco@users.noreply.github.com>
2024-05-03 16:03:35 +0200

    adding a PageTitle component

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,9 @@
+const PageTitle = ({ children }) => {
+  return (
+    <h1 className="text-pink-300 text-3xl font-semibold mb-6 sm:text-6xl">
+      {children}
+    </h1>
+  );
+};
+
+export default PageTitle;
